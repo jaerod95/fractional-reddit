cadd5b9c569df867c844e87a077d6f70518d5ca5
Jason Rodriguez <jason.eli.rodriguez@gmail.com>
2022-05-17 09:09:21 -0700

    Adds basic buttons

--- MemesApp/PostTableViewCell.swift
+++ MemesApp/PostTableViewCell.swift
@@ -15,6 +15,11 @@ class PostTableViewCell: UITableViewCell {
     @IBOutlet private var postImage: UIImageView!
     @IBOutlet private var titleLabel: UILabel!
     @IBOutlet private var captionLabel: UILabel!
+    @IBOutlet private var authorImage: UIImageView!
+    @IBOutlet private var upvote: VerticalButton!
+    @IBOutlet private var comments: VerticalButton!
+    @IBOutlet private var share: VerticalButton!
+    
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -34,5 +39,25 @@ class PostTableViewCell: UITableViewCell {
         self.titleLabel.text = post.author
         self.captionLabel.text = post.title
         self.backgroundColor = .black
+    }
+}
+
+class VerticalButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentHorizontalAlignment = .left
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        centerButtonImageAndTitle()
+    }
+    
+    private func centerButtonImageAndTitle() {
+        let titleSize = self.titleLabel?.frame.size ?? .zero
+        let imageSize = self.imageView?.frame.size  ?? .zero
+        let spacing: CGFloat = 6.0
+        self.imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing),left: 0, bottom: 0, right:  -titleSize.width)
+        self.titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageSize.width, bottom: -(imageSize.height + spacing), right: 0)
     }
 }
